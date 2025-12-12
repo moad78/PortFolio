de97ab71c1bbf70dc3c9124153c7aa10b2a11bd3
moad78 <seiseymoad@gmail.com>
2025-12-12 18:12:21 +0100

    Delete js/app.js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector('#menu-toggle');
-const nav = document.querySelector('nav');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    menuToggle.classList.toggle('open');
-});
